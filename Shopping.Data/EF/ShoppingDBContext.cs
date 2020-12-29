6267426e19de98661ef8a075bcdb2558708fb276
NguyenAnh11 <60679961+NguyenAnh11@users.noreply.github.com>
2020-12-29 09:37:10 +0700

    Add entity

---
+++ Shopping.Data/EF/ShoppingDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopping.Data.EF
+{
+    public class ShoppingDBContext : DbContext
+    {
+        public ShoppingDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+    }
+}
