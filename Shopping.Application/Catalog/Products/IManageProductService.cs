96e24386efdc73e283e7fa6f695151e6ea3c5332
anhnguyen123nthn <anhnguyen123nthn@outlook.com.vn>
2020-12-31 21:05:19 +0700

    Add Product Service

---
+++ Shopping.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Shopping.Application.Catalog.Products.Dtos;
+using Shopping.Application.Catalog.Products.Dtos.Manage;
+using Shopping.Application.Dtos;
+
+namespace Shopping.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductEditRequest request);
+        Task<int> Delete(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdateStock(int productId, int newQuantity);
+        Task AddViewCount(int productId);
+        Task<PageResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+    }
+}
---
+++ Shopping.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Shopping.Application.Catalog.Products.Dtos;
+using Shopping.Application.Catalog.Products.Dtos.Public;
+using Shopping.Application.Dtos;
+
+namespace Shopping.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        Task<PageResult<ProductViewModel>> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
